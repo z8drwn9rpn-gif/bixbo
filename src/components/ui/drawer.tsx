54fcc2df1d8089192ad354ef7881028f956cdbe6
z8drwn9rpn-gif <z8drwn9rpn@privaterelay.appleid.com>
2026-08-09 22:08:35 +0200

    feat: soft sheet/drawer motion + global fade CSS utilities

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -23,7 +23,7 @@ const DrawerOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DrawerPrimitive.Overlay
     ref={ref}
-    className={cn("fixed inset-0 z-50 bg-black/80", className)}
+    className={cn("fixed inset-0 z-50 bg-black/45 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0", className)}
     {...props}
   />
 ));
