b285d6649f9c7ab42a131329ffb55d4e6e9a1f84
z8drwn9rpn-gif <z8drwn9rpn@privaterelay.appleid.com>
2026-08-10 07:08:58 +0200

    Update notifications.tsx

--- src/routes/notifications.tsx
+++ src/routes/notifications.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { useEffect, useState, type ReactNode } from "react";
-import { Bell, BellOff, ChevronLeft, Moon } from "lucide-react";
+import { Bell, BellOff, ChevronLeft, Moon } from "@/components/icons/BixboIcons";
 
 import { AppShell } from "@/components/AppShell";
 import { Switch } from "@/components/ui/switch";
