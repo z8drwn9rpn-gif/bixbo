8a6a592727e28850d539723b8190468ebc588def
z8drwn9rpn-gif <z8drwn9rpn@privaterelay.appleid.com>
2026-08-10 08:13:28 +0200

    Update sheet.tsx

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import * as SheetPrimitive from "@radix-ui/react-dialog";
 import { cva, type VariantProps } from "class-variance-authority";
-import { X } from "lucide-react";
+import { X } from "@/components/icons/BixboIcons";
 
 import { cn } from "@/lib/utils";
 
@@ -58,7 +58,7 @@ const SheetContent = React.forwardRef<React.ElementRef<typeof SheetPrimitive.Con
       <SheetOverlay />
       <SheetPrimitive.Content ref={ref} className={cn(sheetVariants({ side }), className)} {...props}>
         <SheetPrimitive.Close className="absolute right-4 top-4 rounded-full p-2 opacity-80 ring-offset-background cursor-pointer transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-          <X className="h-4 w-4" />
+          <X size={18} />
           <span className="sr-only">Close</span>
         </SheetPrimitive.Close>
         {children}
