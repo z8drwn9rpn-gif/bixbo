4d11f05f89ce31b4e82bbf176a773b9076d74ddf
z8drwn9rpn-gif <z8drwn9rpn@privaterelay.appleid.com>
2026-08-12 11:02:49 +0200

    Use finalized BIXBO Health Report PDF component

--- src/routes/report.tsx
+++ src/routes/report.tsx
@@ -1,4 +1,4 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { HealthReportPage } from "@/components/HealthReportPage";
+import { HealthReportPageFinal } from "@/components/HealthReportPageFinal";
 
-export const Route = createFileRoute("/report")({ component: HealthReportPage });
+export const Route = createFileRoute("/report")({ component: HealthReportPageFinal });
