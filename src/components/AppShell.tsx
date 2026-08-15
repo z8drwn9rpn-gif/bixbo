c06e187228d610c3422d36b47aa00e99bdab6042
z8drwn9rpn-gif <z8drwn9rpn@privaterelay.appleid.com>
2026-08-15 02:06:37 +0200

    Expose BIXBO icon keyboard across app text fields

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -1,6 +1,7 @@
 import { type ReactNode } from "react";
 import { BottomNav } from "./BottomNav";
 import { SideNav } from "./SideNav";
+import { BixboIconKeyboard } from "./icons/BixboIconKeyboard";
 import bixboMascot from "@/assets/bixbo-mascot-user.png";
 import "@/ui-polish.css";
 import "@/ui-components-polish.css";
@@ -31,6 +32,7 @@ export function AppShell({ children, title, right, big = false }: { children: Re
           <main id="main-content" tabIndex={-1} className="bixbo-page-fade min-w-0 overflow-x-hidden outline-none">{children}</main>
         </div>
       </div>
+      <BixboIconKeyboard />
       <BottomNav />
     </div>
   );
